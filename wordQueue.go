591b8a74fadbbdc0ac1cf46c76dbaf3ddb81e21d
Garrett Brenner <garrebrenna@gmail.com>
2020-04-01 17:55:32 -0400

    concurrency working, cleaned up

--- wordQueue.go
+++ wordQueue.go
@@ -5,9 +5,6 @@ import "os"
 import "strconv"
 import "strings"
 
-var numTasks int
-const signal = "0123456789qwertyuioplkjhgfdsazxcvbnm"
-
 func countWords(str string) int {
 	//accepts a line
 	//count and return all the words in the line
@@ -24,6 +21,9 @@ func task(id int, jobs <-chan string, results chan<- int) {
 }
 
 func main() {
+	var numTasks int
+	var numLines int
+
 	args := os.Args[1:]
 	if len(args) != 1 {
 		fmt.Println("USAGE: Wrong number of arguments.")
@@ -40,10 +40,16 @@ func main() {
 		os.Exit(0)
 	}
 
+	example := [...]string{"This is the first line",
+			"This is the second line of course",
+			"Naturally this is the third line, what else",
+			"It follows by extrapolation that this line represents the fourth in the continuum of lines"}
+	numLines = len(example)
+
 	//create buffered channels
 	//use the number of lines to determine buffer size (or don't use a buffer, idk)
-	jobs := make(chan string)
-	results := make(chan int)
+	jobs := make(chan string, numLines)
+	results := make(chan int, numLines)
 
 	//start tasks
 	for i := 0; i < numTasks; i++ {
@@ -51,19 +57,13 @@ func main() {
 	}
 
 	//add everything to the queue
-	//enqueue a special string to signal the end
-	example := [...]string{"This is the first line",
-			"This is the second line of course",
-			"Naturally this is the third line, what else",
-			"It follows by extrapolation that this line represents the fourth in the continuum of lines"}
-
-	for i := 0; i < len(example); i++ {
+	for i := 0; i < numLines; i++ {
 		jobs <- example[i]
 	}
 	close(jobs)
 
-	for i := range results {
-		fmt.Println(i)
+	for i := 0; i < numLines; i++ {
+		fmt.Println(<-results)
 	}
 
 }
