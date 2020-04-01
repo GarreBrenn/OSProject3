6d4630055c187b4d1d86fc6503543451d64e3dd9
Garrett Brenner <garrebrenna@gmail.com>
2020-04-01 14:23:25 -0400

    added exits

--- wordQueue.go
+++ wordQueue.go
@@ -7,10 +7,17 @@ import "strconv"
 var numTasks int
 const signal = "0123456789qwertyuioplkjhgfdsazxcvbnm"
 
-func task(id int) {
-	fmt.Println("id: ",id)
-	//dequeue off the queue
-	//check to see if the line is the special string
+func countWords(str string) {
+	//accepts a line
+	//count and return all the words in the line
+}
+
+func task(id int, jobs <-chan string, results chan<- int) {
+	fmt.Println("I am id: ",id)
+	for n := range jobs {
+		fmt.Println(n);
+		results <- 4
+	}
 }
 
 func main() {
@@ -18,6 +25,7 @@ func main() {
 	if len(args) != 1 {
 		fmt.Println("USAGE: Wrong number of arguments.")
 		fmt.Println("Please enter the (int) number of consumer tasks to run.\n")
+		os.Exit(0)
 	}
 
 	if num, err := strconv.Atoi(args[0]); err == nil {
@@ -26,12 +34,18 @@ func main() {
 	} else {
 		fmt.Println("USAGE: Wrong argument type.")
 		fmt.Println("Please enter the (int) number of consumer tasks to run.\n")
+		os.Exit(0)
 	}
+
+	//create buffered channels
+	//use the number of lines to determine buffer size (or don't use a buffer, idk)
+	jobs := make(chan string)
+	results := make(chan int)
 
 	//start tasks
 	for i := 0; i < numTasks; i++ {
-		go task(i)
-		fmt.Println("asdf")
+		go task(i, jobs, results)
+		fmt.Println("Starting task: ", i)
 	}
 
 	//add everything to the queue
