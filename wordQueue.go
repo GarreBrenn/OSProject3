5280c4bfe0c6decf58ac89b72b8c9d187eb28939
Garrett Brenner <garrebrenna@gmail.com>
2020-04-01 02:40:16 -0400

    command line arguments handled

--- wordQueue.go
+++ wordQueue.go
@@ -2,14 +2,22 @@ package main
 
 import "fmt"
 import "os"
+import "strconv"
 
+var numTasks int
 
 func main() {
 	args := os.Args[1:]
 	if len(args) != 1 {
 		fmt.Println("USAGE: Wrong number of arguments.")
-		fmt.Println("Please enter the number of consumer tasks to run.\n")
+		fmt.Println("Please enter the (int) number of consumer tasks to run.\n")
+	}
+
+	if num, err := strconv.Atoi(args[0]); err == nil {
+		numTasks = num
+		fmt.Println(numTasks + 5)
 	} else {
-		fmt.Println("Congrats!")
+		fmt.Println("USAGE: Wrong argument type.")
+		fmt.Println("Please enter the (int) number of consumer tasks to run.\n")
 	}
 }
