2abbf4c7f28eb7919eef5495b74fdc3be2bc1248
Garrett Brenner <garrebrenna@gmail.com>
2020-04-01 04:01:00 -0400

    done for tonight

--- wordQueue.go
+++ wordQueue.go
@@ -5,6 +5,13 @@ import "os"
 import "strconv"
 
 var numTasks int
+const signal = "0123456789qwertyuioplkjhgfdsazxcvbnm"
+
+func task(id int) {
+	fmt.Println("id: ",id)
+	//dequeue off the queue
+	//check to see if the line is the special string
+}
 
 func main() {
 	args := os.Args[1:]
@@ -15,9 +22,18 @@ func main() {
 
 	if num, err := strconv.Atoi(args[0]); err == nil {
 		numTasks = num
-		fmt.Println(numTasks + 5)
+		fmt.Println("numTasks: ", numTasks)
 	} else {
 		fmt.Println("USAGE: Wrong argument type.")
 		fmt.Println("Please enter the (int) number of consumer tasks to run.\n")
 	}
+
+	//start tasks
+	for i := 0; i < numTasks; i++ {
+		go task(i)
+		fmt.Println("asdf")
+	}
+
+	//add everything to the queue
+	//enqueue a special string to signal the end
 }
